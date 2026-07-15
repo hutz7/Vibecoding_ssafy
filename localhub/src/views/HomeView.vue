77fda0960fc8c81bdde07062b128a5d91e5a7296
배기태 <qorlxo1@naver.com>
2026-07-15 16:27:24 +0900

    4시27분작업

---
+++ localhub/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <h1>🏙️ LocalHub</h1>
+
+    <p>서울 지역 관광 정보를 한눈에 제공합니다.</p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 50px;
+  text-align: center;
+}
+
+h1 {
+  color: #1976d2;
+}
+
+p {
+  color: gray;
+}
+</style>
