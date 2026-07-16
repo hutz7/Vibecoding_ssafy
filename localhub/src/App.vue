41a7c9db09ea0de89369093b524db76967ecd65e
배기태 <qorlxo1@naver.com>
2026-07-16 09:19:17 +0900

    합병입니다

--- localhub/src/App.vue
+++ localhub/src/App.vue
@@ -1,25 +1,9 @@
-<script setup>
-import Header from '@/components/common/Header.vue'
-import Footer from '@/components/common/Footer.vue'
-</script>
+<script setup></script>
 
 <template>
-  <Header />
-
-  <main class="container">
-    <RouterView />
-  </main>
-
-  <Footer />
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
-
-<style>
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.container {
-  min-height: calc(100vh - 130px);
-}
-</style>
